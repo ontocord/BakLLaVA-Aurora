c94e826fe3589ed4f5df0471963576d2a63737d3
ontocord <ontocord@gmail.com>
2023-12-06 12:08:20 -0800

    Update builder.py

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -133,7 +133,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
             vision_tower.load_model()
         vision_tower.to(device='cuda', dtype=torch.float16)
         image_processor = vision_tower.image_processor
-        text_tokenizer = vision_tower.text_tokenizer
+        text_tokenizer = vision_tower.text_tokenizer #NOTE: VERY HACKY. SHOULD BE LOADED BY NAME
     elif 'llava' in model_name.lower():
         mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
         mm_use_im_patch_token = getattr(model.config, "mm_use_im_patch_token", True)
