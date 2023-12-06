913ed1e730b08ee28adfbeed63311dec9b25cbf2
ontocord <ontocord@gmail.com>
2023-12-06 12:12:11 -0800

    Update builder.py

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -2,7 +2,7 @@ import os
 from .clip_encoder import CLIPVisionTower
 
 
-def build_vision_tower(vision_tower_cfg, **kwargs):
+def build_multimodal_towers(vision_tower_cfg, **kwargs):
     vision_tower = getattr(vision_tower_cfg, 'mm_vision_tower', getattr(vision_tower_cfg, 'vision_tower', None))
     is_absolute_path_exists = os.path.exists(vision_tower)
     text_tower = None
