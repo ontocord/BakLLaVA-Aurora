2fff1f57ab0221ac33165954c9d63741434cab74
ontocord <ontocord@gmail.com>
2023-12-16 12:22:52 -0600

    Update llava_aurora.py

--- llava/model/language_model/llava_aurora.py
+++ llava/model/language_model/llava_aurora.py
@@ -81,7 +81,7 @@ class LlavaAuroraForCausalLM(GPTBigCodeForCausalLM, LlavaMetaForCausalLM):
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
+        print (text_toks)
         if images is not None or text_toks is not None:
             input_ids, attention_mask, past_key_values, inputs_embeds, labels = self.prepare_inputs_labels_for_multimodal(input_ids, attention_mask, past_key_values, labels, images, text_toks)
         # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
